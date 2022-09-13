ec7e868f18de4237ce4b05e5b993d645ad392174
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-09-14 01:04:59 +1200

    unit test for deck

---
+++ TestGreenField/TestDec.cs
@@ -0,0 +1,44 @@
+﻿using GreenField;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestGreenField
+{
+    public class TestDec
+    {
+        [Fact]
+        public void ShoudInitialiizeFourSuitesOfCards()
+        {
+            //Arrange
+            Deck deck = new Deck();
+
+            //Act
+            List<Card> cards_repo = deck.Repository;
+            //Assert
+
+            Assert.NotEmpty(cards_repo);
+        }
+
+        [Fact]
+        public void ShouldShuffleTheCards()
+        {
+            //Arrange
+            Deck deck = new Deck();
+
+            //Act
+            List<Card> cards_repo = new List<Card>();
+            cards_repo.AddRange(deck.Repository);
+
+           var shuffledCards = deck.Shuffle();
+
+            //Assert
+
+            Assert.NotEqual<List<Card>>(cards_repo, shuffledCards);
+        }
+
+    }
+}
