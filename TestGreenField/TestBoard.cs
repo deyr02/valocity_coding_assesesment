3fe5ffe5e9145179273f4fab06104df253a52809
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-09-14 01:37:30 +1200

    unitTest Board

---
+++ TestGreenField/TestBoard.cs
@@ -0,0 +1,25 @@
+﻿using GreenField;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestGreenField
+{
+    public class TestBoard
+    {
+        public void PlayersCardShouldNotBeIdentical()
+        {
+            //Arrange
+            Board board = new Board();
+            //Act
+            board.ShaffleCards();
+            board.DistributeCards();
+
+            //Assert
+            Assert.NotEqual<List<Card>>(board.Players[0].Cards, board.Players[1].Cards);
+        }
+    }
+}
