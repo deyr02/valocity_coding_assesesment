435082126abda0f20af0d671f3a6d4fb339eb0fb
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-09-14 01:26:58 +1200

    Player class added

---
+++ GreenField/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace GreenField
+{
+    public class Player
+    {
+
+        public string Name { get; set; }
+        public List<Card> Cards { get; set; }
+        public Player(string name)
+        {
+            this.Name = name;
+            Cards = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            List<Card> temp = Cards;
+            temp.Add(card);
+            Cards = temp;
+        }
+    }
+}
--- TestGreenField/TestPlayer.cs
+++ TestGreenField/TestPlayer.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using GreenField;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TestGreenField
 {
-    class Player
+    public class TestPlayer
     {
+        [Theory]
+        [InlineData("Bob", "Bob")]
+        public void ShouldInitializePlayerName(string playerName, string exptectedName)
+        {
+            //Arrange
+            Player player = new Player(playerName);
+
+            //Act
+            string _name = player.Name;
+            //Assert
+            Assert.Equal(exptectedName, _name);
+        }
+
+        [Theory]
+        [InlineData("Bob", "Heart", "A")]
+        public void ShouldAddNewCardToUser(string playerName, string suiteName, string cardName)
+        {
+            //Arrange
+            Player player = new Player(playerName);
+
+            //Act
+            Card card = new Card(suiteName, cardName);
+            player.AddCard(card);
+            //Assert
+            Assert.Equal<Card>(card, player.Cards[player.Cards.Count -1]);
+        }
     }
 }
