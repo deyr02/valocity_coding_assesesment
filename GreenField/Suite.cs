8ce5113e6786aa7709a5f1cb355c9da74aaf1b1c
DEY RAJIB <rade002@ess.ais.ac.nz>
2022-09-14 00:04:51 +1200

    suite class added

---
+++ GreenField/Suite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GreenField
+{
+    public class Suite
+    {
+        public string Name { get; set; }
+        public List<Card> Cards { get; private set; }
+
+
+        public Suite(string name)
+        {
+            this.Name = name;
+            this.InitializeCards();
+        }
+
+        private string[] Positions => new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+        private void InitializeCards()
+        {
+            List<Card> temp = new List<Card>();
+            foreach (var position in Positions)
+            {
+                temp.Add(new Card(this.Name, position));
+            }
+            this.Cards = temp;
+        }
+    }
+}
--- TestGreenField/TestSuite.cs
+++ TestGreenField/TestSuite.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using GreenField;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TestGreenField
 {
-    class TestSuite
+    public class TestSuite
     {
+        [Theory]
+        [InlineData("Heart", "Heart")]
+        [InlineData("Diamond", "Diamond")]
+        [InlineData("Spade", "Spade")]
+        [InlineData("Club", "Club")]
+        public void ShouldInitializeSuiteName(string suiteName, string expectedSuiteName)
+        {
+            //Arange
+            Suite suite = new Suite(suiteName);
+            //Act
+            string _suiteName = suite.Name;
+            //Assert
+            Assert.Equal(expectedSuiteName, _suiteName);
+            
+        }
     }
 }
